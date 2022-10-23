052994ecec950a70a1bd238dd519db8c97e67ff0
BB-Kang <kanghm0804@gmail.com>
2022-10-23 20:47:04 +0900

    feat(add navigation)

---
+++ lib/index_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  const IndexPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(
+            "운동 시설",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.yellow,
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () => {print("hi1")},
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () => {print("hi2")},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => {print("hi3")},
+            ),
+          ],
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:make_some_screens/index_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -77,53 +77,63 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      // body: Center(
-      //   // Center is a layout widget. It takes a single child and positions it
-      //   // in the middle of the parent.
-      //   child: Column(
-      //     // Column is also a layout widget. It takes a list of children and
-      //     // arranges them vertically. By default, it sizes itself to fit its
-      //     // children horizontally, and tries to be as tall as its parent.
-      //     //
-      //     // Invoke "debug painting" (press "p" in the console, choose the
-      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
-      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-      //     // to see the wireframe for each widget.
-      //     //
-      //     // Column has various properties to control how it sizes itself and
-      //     // how it positions its children. Here we use mainAxisAlignment to
-      //     // center the children vertically; the main axis here is the vertical
-      //     // axis because Columns are vertical (the cross axis would be
-      //     // horizontal).
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       const Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headline4,
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      body: const WebView(
-        initialUrl: 'https://',
+      body:
+          // Center(
+
+          //   // Center is a layout widget. It takes a single child and positions it
+          //   // in the middle of the parent.
+          //   child: Column(
+          //     // Column is also a layout widget. It takes a list of children and
+          //     // arranges them vertically. By default, it sizes itself to fit its
+          //     // children horizontally, and tries to be as tall as its parent.
+          //     //
+          //     // Invoke "debug painting" (press "p" in the console, choose the
+          //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          //     // to see the wireframe for each widget.
+          //     //
+          //     // Column has various properties to control how it sizes itself and
+          //     // how it positions its children. Here we use mainAxisAlignment to
+          //     // center the children vertically; the main axis here is the vertical
+          //     // axis because Columns are vertical (the cross axis would be
+          //     // horizontal).
+
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: <Widget>[
+          //       const Text(
+          //         'You have pushed the button this many times:',
+          //       ),
+          //       Text(
+          //         '$_counter',
+          //         style: Theme.of(context).textTheme.headline4,
+          //       ),
+          //     ],
+
+          //   ),
+
+          // ),
+          Center(
+        child: ElevatedButton(
+            child: const Text('비회원 접속'),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const IndexPage()));
+            }),
       ),
-      floatingActionButton: Padding(
-          padding: EdgeInsets.all(40),
-          child: Row(children: [
-            FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            )
-          ])), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: Row(
+      //   children: [
+      //     FloatingActionButton(
+      //       onPressed: _incrementCounter,
+      //       tooltip: 'Increment',
+      //       child: const Icon(Icons.add),
+      //     ),
+      //     FloatingActionButton(
+      //       onPressed: _incrementCounter,
+      //       tooltip: 'Increment',
+      //       child: const Icon(Icons.add),
+      //     ),
+      //   ],
+      // ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

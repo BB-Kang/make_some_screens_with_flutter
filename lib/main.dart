8cd54804a66808cffd0e01509dda195326b63f25
DaehyeongLee <apm5018@naver.com>
2022-10-23 21:23:59 +0900

    feat. Landing page add

--- lib/main.dart
+++ lib/main.dart
@@ -65,75 +65,84 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/background.jpeg'))),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Go Fit',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                '하나의 패스로 관리하는 나의 피트니스',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: SizedBox(
+                  width: 250,
+                  height: 40,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightGreen),
+                      child: const Text('회원가입'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const IndexPage()));
+                      }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 7),
+                child: SizedBox(
+                  width: 250,
+                  height: 40,
+                  child: ElevatedButton(
+                      child: const Text('로그인'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const IndexPage()));
+                      }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: 250,
+                  height: 40,
+                  child: ElevatedButton(
+                      child: const Text('비 회원'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const IndexPage()));
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body:
-          // Center(
-
-          //   // Center is a layout widget. It takes a single child and positions it
-          //   // in the middle of the parent.
-          //   child: Column(
-          //     // Column is also a layout widget. It takes a list of children and
-          //     // arranges them vertically. By default, it sizes itself to fit its
-          //     // children horizontally, and tries to be as tall as its parent.
-          //     //
-          //     // Invoke "debug painting" (press "p" in the console, choose the
-          //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          //     // to see the wireframe for each widget.
-          //     //
-          //     // Column has various properties to control how it sizes itself and
-          //     // how it positions its children. Here we use mainAxisAlignment to
-          //     // center the children vertically; the main axis here is the vertical
-          //     // axis because Columns are vertical (the cross axis would be
-          //     // horizontal).
-
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: <Widget>[
-          //       const Text(
-          //         'You have pushed the button this many times:',
-          //       ),
-          //       Text(
-          //         '$_counter',
-          //         style: Theme.of(context).textTheme.headline4,
-          //       ),
-          //     ],
-
-          //   ),
-
-          // ),
-          Center(
-        child: ElevatedButton(
-            child: const Text('비회원 접속'),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const IndexPage()));
-            }),
-      ),
-      // floatingActionButton: Row(
-      //   children: [
-      //     FloatingActionButton(
-      //       onPressed: _incrementCounter,
-      //       tooltip: 'Increment',
-      //       child: const Icon(Icons.add),
-      //     ),
-      //     FloatingActionButton(
-      //       onPressed: _incrementCounter,
-      //       tooltip: 'Increment',
-      //       child: const Icon(Icons.add),
-      //     ),
-      //   ],
-      // ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

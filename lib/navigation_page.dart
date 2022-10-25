414b8bc0723928d5732c09ebea6773593b6650f4
ske-kr <54828276+ske-kr@users.noreply.github.com>
2022-10-25 13:06:27 +0900

    feat:navigation page add color selected

--- lib/navigation_page.dart
+++ lib/navigation_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:make_some_screens/memberships/member_page.dart';
 import 'package:make_some_screens/reservations/reservation_page.dart';
@@ -37,6 +39,8 @@ class _HomeState extends State<Home> {
   final _navigatorKeyList =
       List.generate(5, (index) => GlobalKey<NavigatorState>());
   int _currentIndex = 0;
+  static const defaultColor = Colors.blueAccent;
+  static const selectedColor = Colors.amberAccent;
 
   @override
   Widget build(BuildContext context) {
@@ -67,28 +71,32 @@ class _HomeState extends State<Home> {
             onTap: (index) => setState(() {
               _currentIndex = index;
             }),
-            tabs: const [
+            tabs: [
               Tab(
-                icon: Icon(Icons.fitness_center, color: Colors.blueAccent),
+                icon: Icon(Icons.fitness_center,
+                    color: _currentIndex == 0 ? selectedColor : defaultColor),
                 text: '운동시설',
               ),
               Tab(
-                icon: Icon(Icons.check_box, color: Colors.blueAccent),
+                icon: Icon(Icons.check_box,
+                    color: _currentIndex == 1 ? selectedColor : defaultColor),
                 text: '예약현황',
               ),
               Tab(
                 icon: Icon(
                   Icons.shopping_basket,
-                  color: Colors.blueAccent,
+                  color: _currentIndex == 2 ? selectedColor : defaultColor,
                 ),
                 text: '멤버십',
               ),
               Tab(
-                icon: Icon(Icons.stars, color: Colors.blueAccent),
+                icon: Icon(Icons.stars,
+                    color: _currentIndex == 3 ? selectedColor : defaultColor),
                 text: '인플루언서',
               ),
               Tab(
-                icon: Icon(Icons.account_circle, color: Colors.blueAccent),
+                icon: Icon(Icons.account_circle,
+                    color: _currentIndex == 4 ? selectedColor : defaultColor),
                 text: 'Profile',
               ),
             ],
